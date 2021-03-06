64078ea6678a9f9f6bcc637ee402112c6abce142
Lior Stern <liorst4@gmail.com>
2021-03-06 14:14:07 +0200

    license: Add notices to source files.

--- app/Interpreter.hs
+++ app/Interpreter.hs
@@ -1,3 +1,24 @@
+{-|
+    rpn_calculator is a reverse polish notation calculator.
+
+    Copyright (C) 2021  Lior Stern
+
+    This file is part of rpn_calculator.
+
+    rpn_calculator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    rpn_calculator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with rpn_calculator.  If not, see <https://www.gnu.org/licenses/>.
+-}
+
 module Interpreter where
 
 import Data.Either
